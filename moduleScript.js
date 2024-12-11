3517533b2e3958ee33d5bfc899241b2967c0d930
Toby Roworth <toby.roworth@gmail.com>
2024-12-10 21:47:56 -0500

    feat: add crossfade function
    adds init, update, and fade funcions to support crossfade function

--- moduleScript.js
+++ moduleScript.js
@@ -1,3 +1,38 @@
+var activeFades = [];
+
+var now = 0;
+
+function init() {
+  now = util.getTimestamp() * 1000;
+  script.setUpdateRate(127);
+}
+
+function update(deltaTime) {
+  script.log("Delta time : " + deltaTime);
+  now += deltaTime * 1000;
+  for (var i = 0; i < activeFades.length; i++) {
+    var fade = activeFades[i];
+    if (fade) {
+      var currentTime = now - fade.startTime;
+      if (currentTime < 0) {
+        continue;
+      }
+      var totalTime = Math.max(fade.endTime - fade.startTime, 1);
+      var completion = Math.min(currentTime / totalTime, 1);
+      var newValue = fade.startValue + (completion * (fade.endValue - fade.startValue));
+      newValue = Math.round(newValue);
+      if (newValue == fade.value) {
+        continue;
+      }
+      fade.value = newValue;
+      if (newValue == fade.endValue) {
+        activeFades[i] = null;
+      }
+      setLevel(0, i, newValue);
+    }
+  }
+}
+
 function setLevel(channelType, channelNumber, level) {
 
   if (channelType != 0) {
@@ -5,4 +40,21 @@ function setLevel(channelType, channelNumber, level) {
   }
 
   local.sendCC(local.parameters.midiChannel.send.get(), channelNumber, level);
+}
+
+function fade(channelNumber, start, end, timeMs, delayMs) {
+  var now = util.getTimestamp() * 1000;
+  activeFades[channelNumber] = {
+    startTime: now + delayMs,
+    endTime: now + delayMs + timeMs,
+    startValue: start,
+    endValue: end,
+    value: null
+  };
+}
+
+function crossfade(fromChannel, toChannel, time) {
+  var timeMs = time * 100;
+  fade(fromChannel, 0, 127, timeMs, 0);
+  fade(toChannel, 127, 0, timeMs, timeMs);
 }
